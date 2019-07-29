66dfb9e1b905e4604c9ad3d80374707109454c47
Youri Haenen <y.h.b.haenen@student.tue.nl>
2019-07-29 11:06:51 +0200

    Fixed non-closing menu bar on mobile

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,29 +3,29 @@
         <div class="stripe--red"></div>
         <nav class="navbar navbar-expand-lg navbar-light bg-yellow">
             <a class="navbar-brand" href="#">TCL</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainNavigation"
+                    aria-controls="mainNavigation" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="collapse navbar-collapse" id="mainNavigation">
                 <ul class="navbar-nav ml-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#" v-scroll-to="'#home'">Intro <span class="sr-only">(current)</span></a>
+                    <li class="nav-item active" data-toggle="collapse" data-target=".navbar-collapse.show">
+                        <a class="nav-link" href="#" v-scroll-to="'#home'">Intro</a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
                         <a class="nav-link" href="#" v-scroll-to="'#tuna'">De Tuna</a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
                         <a class="nav-link" href="#" v-scroll-to="'#muziek'">Muziek</a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
                         <a class="nav-link" href="#" v-scroll-to="'#gezelligheid'">Gezelligheid</a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
                         <a class="nav-link" href="#" v-scroll-to="'#studeren'">Studeren</a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
                         <a class="nav-link" href="#" v-scroll-to="'#contact'">Contact</a>
                     </li>
                 </ul>
